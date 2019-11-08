3e508851cdc535617057ab1fc50b886924b83960
Klaus Hörmann-Engl <hoermann.klaus@gmail.com>
2019-11-08 21:36:51 +0100

    [BUGFIX] Fixing error in header includes

--- assets/partials/headerIncludes.php
+++ assets/partials/headerIncludes.php
@@ -1,4 +1,4 @@
-    link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
     <link rel="stylesheet" href="external/bootstrap/bootstrap.css" type="text/css">
     <script src="external/jquery/jquery-3.4.1.min.js"></script>
     <script src="external/bootstrap/bootstrap.js"></script>
