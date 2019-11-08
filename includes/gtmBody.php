79cf0d4d0651907ac3c0fdb591f19747bf6ac227
Klaus Hörmann-Engl <hoermann.klaus@gmail.com>
2019-11-08 22:41:09 +0100

    [TASK] Changed GTM container ID

--- includes/gtmBody.php
+++ includes/gtmBody.php
@@ -1,4 +1,4 @@
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKD24WH"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4W5QK7"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
