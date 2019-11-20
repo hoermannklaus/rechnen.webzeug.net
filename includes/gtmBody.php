0d91f4a1411fa6e9b2e13a7ee3993c08a278dc4c
Klaus Hörmann-Engl <hoermann.klaus@gmail.com>
2019-11-20 20:36:13 +0100

    [TASK] New container for rechnen

--- includes/gtmBody.php
+++ includes/gtmBody.php
@@ -1,4 +1,4 @@
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4W5QK7"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MRL6QLK"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
